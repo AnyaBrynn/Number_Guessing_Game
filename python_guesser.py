b442f320a034407f61b49ad79dd83cf02e962396
Anya Stiefbold <2598511s@student.gla.ac.uk>
2022-09-24 08:44:35 +0100

    Clean up spacing

--- python_guesser.py
+++ python_guesser.py
@@ -3,12 +3,11 @@ import random
 # computer chooses number 
 secretNumber = random.randint(1,99)
 
-
 # get player number
 playerName = input("Hello what is your name? ")
 
 # user guesses a number 
-guess = int(input("I'm thinking of a number between 1 and 99\nGuess a number: "))
+guess = int(input("I'm thinking of a number between 1 and 99\nYou have 7 tries, guess a number: "))
 numberOfGuesses = 1
 
 # check if guess is correct 
@@ -22,8 +21,6 @@ while numberOfGuesses < 7:
     if guess == secretNumber:
         break
     numberOfGuesses += 1
-    print(numberOfGuesses)
-
     
 if guess == secretNumber:
     print("Congrats " + playerName + " you guessed it!\nIt took you " + str(numberOfGuesses) + " guesses")

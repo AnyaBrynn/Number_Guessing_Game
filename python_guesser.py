99a430b287c02e27e1c7efb41ddbd3c98341e7b2
Anya Stiefbold <2598511s@student.gla.ac.uk>
2022-09-24 09:06:44 +0100

    Added input type check

--- python_guesser.py
+++ python_guesser.py
@@ -7,17 +7,32 @@ secretNumber = random.randint(1,99)
 playerName = input("Hello what is your name? ")
 
 # user guesses a number 
-guess = int(input("I'm thinking of a number between 1 and 99\nYou have 7 tries, guess a number: "))
+while True:
+    try:
+        guess = int(input("I'm thinking of a number between 1 and 99\nYou have 7 tries, guess a number: "))
+        break
+    except ValueError:
+        print('Please enter an integer.')
 numberOfGuesses = 1
 
 # check if guess is correct 
 while numberOfGuesses < 7:
     if guess < secretNumber:
         print("Your guess is too low!")
-        guess = int(input("Guess a number between 1 and 99: "))
+        while True:
+            try:
+                guess = int(input("Guess a number between 1 and 99: "))
+                break
+            except ValueError:
+                print('Please enter an integer.')
     if guess > secretNumber:
         print("Your guess is too high!")
-        guess = int(input("Guess a number between 1 and 99: "))
+        while True:
+            try:
+                guess = int(input("Guess a number between 1 and 99: "))
+                break
+            except ValueError:
+                print('Please enter an integer.')
     if guess == secretNumber:
         break
     numberOfGuesses += 1

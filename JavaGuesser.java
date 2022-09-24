9c136d944ec3be9a3158001ba0599ea995951957
Anya Stiefbold <2598511s@student.gla.ac.uk>
2022-09-24 12:00:14 +0100

    Initial game logic

---
+++ JavaGuesser.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class JavaGuesser {
+    
+    public static void guessTheNum() {
+
+        Scanner sc = new Scanner(System.in);
+        // generate secret number 
+        int secretNum = 1 + (int)(Math.random() * ((99 - 1) + 1));
+        int tries = 7;
+        int guess;
+
+        // start the game
+        System.out.println("I'm thinking of a number between 1 and 99\nYou have 7 tries.");
+        for (int i = 0; i < tries; i++) 
+        {
+            System.out.println("Guess a number: ");
+            // user input 
+            guess = sc.nextInt();
+
+            // check the input 
+            if (guess == secretNum)
+            {
+                System.out.println("Congrats you guessed it!");
+                break;
+            }
+            else if (guess > secretNum && i != tries - 1)
+            {
+                System.out.println("Your guess is too high!");
+
+            }
+            else if (guess < secretNum && i != tries - 1)
+            {
+                System.out.println("Your guess is too low!");
+                
+            }
+        }
+    }
+    public static void main(String arg[])
+    {
+        guessTheNum();
+    }
+}
